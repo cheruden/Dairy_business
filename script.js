10c24f4a40745f900b8d3786557e2fda86a3dfd2
cheruden <cheruden25@gmail.com>
2021-03-14 10:19:44 +0300

    adding the computation for the daily, weekly, monthly and yearly income to my page

--- script.js
+++ script.js
@@ -5,8 +5,19 @@ function calculate(){
     var shadeC=document.getElementById("clitre_amount").value;
     var shadeD=document.getElementById("dlitre_amount").value;
     var price=45;
+    var rate=7;
+    var monthy=30;
+    var yearly=12;
 
     var results=((shadeA*price)+(shadeB*price)+(shadeC*price)+(shadeD*price));
-    document.getElementById("total").innerHTML ="KSH" + results;
+    document.getElementById("total").innerHTML ="KSH  " + results;
+    var results2=(((shadeA*price)+(shadeB*price)+(shadeC*price)+(shadeD*price))*rate);
+    document.getElementById("total1").innerHTML ="KSH  " + results2;
+    var results3=(((shadeA*price)+(shadeB*price)+(shadeC*price)+(shadeD*price))*rate*monthy);
+    document.getElementById("total2").innerHTML ="KSH  " +results3;
+    var results4=(((shadeA*price)+(shadeB*price)+(shadeC*price)+(shadeD*price))*rate*monthy*yearly);
+    document.getElementById("total3").innerHTML ="KSH  " +results4;
+
+
 }
      
